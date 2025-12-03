9962eb4e54551ac5f5f52951bc5258744ea68b7a
FabianLars-crabnebula <fabianlars@crabnebula.dev>
2025-12-03 21:15:50 +0100

    feat: add initial plugin config

--- src/error.rs
+++ src/error.rs
@@ -4,18 +4,20 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-  #[error(transparent)]
-  Io(#[from] std::io::Error),
-  #[cfg(mobile)]
-  #[error(transparent)]
-  PluginInvoke(#[from] tauri::plugin::mobile::PluginInvokeError),
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[cfg(mobile)]
+    #[error(transparent)]
+    PluginInvoke(#[from] tauri::plugin::mobile::PluginInvokeError),
+    #[error("No MCPurify config promided in tauri.conf.json or plugin Builder")]
+    MissingConfig,
 }
 
 impl Serialize for Error {
-  fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
-  where
-    S: Serializer,
-  {
-    serializer.serialize_str(self.to_string().as_ref())
-  }
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_ref())
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use tauri::{
-  plugin::{Builder, TauriPlugin},
-  Manager, Runtime,
+    plugin::{Builder as PluginBuilder, TauriPlugin},
+    Manager, Runtime,
 };
 
 pub use models::*;
@@ -23,26 +23,54 @@ use mobile::TauriPluginLlm;
 
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the tauri-plugin-llm APIs.
 pub trait TauriPluginLlmExt<R: Runtime> {
-  fn tauri_plugin_llm(&self) -> &TauriPluginLlm<R>;
+    fn tauri_plugin_llm(&self) -> &TauriPluginLlm<R>;
 }
 
 impl<R: Runtime, T: Manager<R>> crate::TauriPluginLlmExt<R> for T {
-  fn tauri_plugin_llm(&self) -> &TauriPluginLlm<R> {
-    self.state::<TauriPluginLlm<R>>().inner()
-  }
+    fn tauri_plugin_llm(&self) -> &TauriPluginLlm<R> {
+        self.state::<TauriPluginLlm<R>>().inner()
+    }
+}
+
+#[derive(Default)]
+pub struct Builder {
+    mcpurify_config: Option<mcpurify::Config>,
+}
+
+impl Builder {
+    /// Create a new plugin builder
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Programmatically provide a MCPurify config. Overwrites tauri.conf.json.
+    pub fn mcpurify_config(mut self, config: mcpurify::Config) -> Self {
+        self.mcpurify_config = Some(config);
+        self
+    }
+
+    pub fn build<R: Runtime>(self) -> TauriPlugin<R, Option<mcpurify::Config>> {
+        PluginBuilder::<R, Option<mcpurify::Config>>::new("tauri-plugin-llm")
+            .invoke_handler(tauri::generate_handler![commands::ping])
+            .setup(|app, api| {
+                let config = self
+                    .mcpurify_config
+                    .or((*api.config()).clone())
+                    .ok_or(Error::MissingConfig)?;
+
+                #[cfg(mobile)]
+                let tauri_plugin_llm = mobile::init(app, api, config)?;
+                #[cfg(desktop)]
+                let tauri_plugin_llm = desktop::init(app, api, config)?;
+                app.manage(tauri_plugin_llm);
+
+                Ok(())
+            })
+            .build()
+    }
 }
 
 /// Initializes the plugin.
-pub fn init<R: Runtime>() -> TauriPlugin<R> {
-  Builder::new("tauri-plugin-llm")
-    .invoke_handler(tauri::generate_handler![commands::ping])
-    .setup(|app, api| {
-      #[cfg(mobile)]
-      let tauri_plugin_llm = mobile::init(app, api)?;
-      #[cfg(desktop)]
-      let tauri_plugin_llm = desktop::init(app, api)?;
-      app.manage(tauri_plugin_llm);
-      Ok(())
-    })
-    .build()
+pub fn init<R: Runtime>() -> TauriPlugin<R, Option<mcpurify::Config>> {
+    Builder::default().build()
 }
